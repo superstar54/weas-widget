09162c19fab06360715b2e0ca27f6f1a722ec0fc
superstar54 <xingwang1991@gmail.com>
2024-01-23 09:59:49 +0100

    Listen for changes in the 'atoms' property

--- weas_widget/__init__.py
+++ weas_widget/__init__.py
@@ -1,6 +1,7 @@
 import anywidget
 import traitlets
 
+
 class WeasWidget(anywidget.AnyWidget):
     _esm = """
     import * as weas from "https://unpkg.com/weas@0.0.3/dist/weas.mjs";
@@ -9,32 +10,49 @@ class WeasWidget(anywidget.AnyWidget):
         let viewerElement = document.createElement("div");
         viewerElement.style.cssText = "position: relative; width: 500px; height: 500px;";
         el.appendChild(viewerElement);
-        // Use the atoms data from the model
-        const data = getAtomsData();
+        const renderAtoms = () => {
+            const data = model.get("atoms");
+            const atoms = new weas.Atoms(data);
+            const bjs = new weas.BlendJS(viewerElement);
+            const avr = new weas.AtomsViewer(bjs, atoms);
+            bjs.render();
+        };
 
-        const atoms = new weas.Atoms(data);
+
+        // Initial rendering
         setTimeout(() => {
-                const bjs = new weas.BlendJS(viewerElement);
-                const avr = new weas.AtomsViewer(bjs, atoms);
-                bjs.render();
-            }, 10
+            renderAtoms();
+                }, 10
         ); // Delay rendering by 500ms
+
+        // Listen for changes in the 'atoms' property
+        model.on("change:atoms", () => {
+            // Clear the existing content of viewerElement
+            viewerElement.innerHTML = '';
+            // Re-render with the new atoms data
+            renderAtoms();
+        });
+
     }
     """
     atoms = traitlets.Dict().tag(sync=True)
 
+    def from_ase(self, atoms):
+        # Convert an ASE Atoms object to the widget's format
+        species = {}
+        cell = atoms.get_cell().array.flatten().tolist()
+        positions = atoms.get_positions()
+        symbols = atoms.get_chemical_symbols()
+        numbers = atoms.get_atomic_numbers()
+        speciesArray = symbols
+        for i in range(len(symbols)):
+            species[symbols[i]] = [symbols[i], numbers[i]]
+        atoms={"species": species, "cell": cell, "positions": positions, "speciesArray": speciesArray}
+        self.atoms = atoms
+
+    def from_pymatgen(self, structure):
+        # Convert a Pymatgen Structure object to the widget's format
+        atoms_data = self._convert_to_dict(structure)
+        self.atoms = atoms_data
 
 
-
-def show_ase(atoms):
-    species = {}
-    cell = atoms.get_cell().array.flatten().tolist()
-    positions = atoms.get_positions()
-    symbols = atoms.get_chemical_symbols()
-    numbers = atoms.get_atomic_numbers()
-    speciesArray = symbols
-    for i in range(len(symbols)):
-        species[symbols[i]] = [symbols[i], numbers[i]]
-    atoms={"species": species, "cell": cell, "positions": positions, "speciesArray": speciesArray}
-    viewer = WeasWidget(atoms=atoms)
-    return viewer
