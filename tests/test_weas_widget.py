ece07df5caf9bb9c5930c7f3bdf2a4a73097a027
Xing Wang <xingwang1991@gmail.com>
2024-04-03 13:55:09 +0200

    convert the positions (NumPy array) into a list (#51)
    * convert the positions (NumPy array) into a list
    
    * update vector field

--- tests/test_weas_widget.py
+++ tests/test_weas_widget.py
@@ -14,3 +14,27 @@ def test_set_and_get_atoms_ase(h2o):
 
     # Assert
     assert retrieved_atoms == atoms
+
+
+def test_set_and_get_pymatgen_structure():
+    from pymatgen.core import Structure, Lattice
+
+    structure = Structure.from_spacegroup(
+        "Pm-3m", Lattice.cubic(4.1437), ["Cs", "Cl"], [[0, 0, 0], [0.5, 0.5, 0.5]]
+    )
+    viewer = WeasWidget(from_pymatgen=structure)
+    retrieved_structure = viewer.to_pymatgen()
+
+    # Assert
+    assert retrieved_structure == structure
+
+
+def test_set_and_get_pymatgen_molecule():
+    from pymatgen.core import Molecule
+
+    structure = Molecule(["O", "H", "H"], [[0, 0, 0], [0, 0, 1], [0, 1, 0]])
+    viewer = WeasWidget(from_pymatgen=structure)
+    retrieved_structure = viewer.to_pymatgen()
+
+    # Assert
+    assert len(retrieved_structure) == len(structure)
