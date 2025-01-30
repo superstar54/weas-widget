53d35903b41a5cc676589bf9a58d00eac0a76935
superstar54 <xingwang1991@gmail.com>
2025-01-30 11:21:52 +0100

    add test for cell

---
+++ tests/notebooks/tests/cell.test.ts
@@ -0,0 +1,64 @@
+// Modified from ipywidgets unit tests.
+
+
+import { test } from '@jupyterlab/galata';
+
+import { expect } from '@playwright/test';
+
+import * as path from 'path';
+
+test.describe('Cell', () => {
+  test.beforeEach(async ({ page, tmpPath }) => {
+    await page.contents.uploadDirectory(
+      path.resolve(__dirname, './notebooks'),
+      tmpPath
+    );
+    await page.filebrowser.openDirectory(tmpPath);
+  });
+
+  test('Run notebook cell.ipynb and capture cell outputs', async ({
+    page,
+    tmpPath,
+  }) => {
+    const notebook = 'cell.ipynb';
+    await page.notebook.openByPath(`${tmpPath}/${notebook}`);
+    await page.notebook.activate(notebook);
+
+    const captures = new Array<Buffer>();
+    const cellCount = await page.notebook.getCellCount();
+
+    await page.notebook.runCellByCell({
+      onAfterCellRun: async (cellIndex: number) => {
+        let cell = await page.notebook.getCellOutput(cellIndex);
+        const startTime = Date.now();
+        const timeout = 60000; // Timeout in milliseconds, adjust as necessary
+
+        // Polling for cell output to be not null
+        while (!cell && Date.now() - startTime < timeout) {
+          await page.waitForTimeout(3000); // Wait for 3 second before retrying
+          console.log("waiting for cell output to be not null: ", cellIndex);
+          cell = await page.notebook.getCellOutput(cellIndex);
+        }
+
+        if (cell) {
+          captures.push(await cell.screenshot());
+        } else {
+          console.log("Cell output is not available for cell:", cellIndex);
+        }
+      },
+    });
+
+    await page.notebook.save();
+
+    console.log("Cell count:", cellCount);
+    console.log("Captures array length:", captures.length);
+    captures.forEach((capture, index) => {
+      console.log(`Capture[${index}]:`, capture);
+    });
+
+    for (let i = 0; i < cellCount; i++) {
+      const image = `cell-cell-${i}.png`;
+      expect.soft(captures[i]).toMatchSnapshot(image);
+    }
+  });
+});
